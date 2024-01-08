12734df227555658fc5c6ecb9a046db6951e6980
lauralandonlovell <roryboo@gmail.com>
2024-01-08 17:07:38 +0100

    created variables for names.verified.commented out.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name?");
+        string name = Console.ReadLine();
+        // Console.WriteLine(name);
+
+        Console.WriteLine("What is your last name?");
+        string lastName = Console.ReadLine();
+        // Console.WriteLine(lastName);
+
+        Console.WriteLine($"Your name is {lastName}, {name} {lastName}.");
+
+
+
+
+
     }
 }
