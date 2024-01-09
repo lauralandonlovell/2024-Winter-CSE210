5c7c3727349b1a0d654561f03862829bba1d569c
lauralandonlovell <roryboo@gmail.com>
2024-01-09 18:01:38 +0100

    having trouble nesting loops

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,41 +5,44 @@ using System.Net;
 class Program
 {
     static void Main(string[] args)
-    {
-        
+    {   
         //continuing to play
         // string response = "";
+        // int counter = 0;
         // while (response == "yes")
-        //{
-        //importing random number
-        Random random = new Random();
-        int newRandom = random.Next(1,101);
-        Console.WriteLine(newRandom);
-
-        int guess = -1;
-        while (guess != newRandom)
         {
-            Console.Write("Guess a number: ");
-            guess = int.Parse(Console.ReadLine());
+            //importing random number
+            Random random = new Random();
+            int newRandom = random.Next(1,101);
+            Console.WriteLine(newRandom);
 
-            if (newRandom > guess)
+            int guess = -1;
+            while (guess != newRandom)
             {
-                Console.WriteLine("Guess higher next time.");
+                Console.Write("Guess a number: ");
+                guess = int.Parse(Console.ReadLine());
+
+                if (newRandom > guess)
+                {
+                    Console.WriteLine("Guess higher next time.");
+                }
+                else if (newRandom < guess)
+                {
+                    Console.WriteLine("Guess lower next time.");
+                }
+                else
+                {
+                    Console.WriteLine("Congratulations! You guessed the magic number!");
+                    Console.WriteLine("Would you like to play again? yes or no?");
+                    string response = Console.ReadLine();
+                    //counter++;
+                }       
             }
-            else if (newRandom < guess)
-            {
-                Console.WriteLine("Guess lower next time.");
-            }
-            else
-            {
-                Console.WriteLine("Congratulations! You guessed the magic number!"); 
-            }
-            Console.WriteLine("Would you like to play again? yes or no?");    
-        }
             
             //string response = Console.ReadLine();      //while (response == "yes")
             // {
-            //     Console.WriteLine("say hi");
+        }   //     Console.WriteLine("say hi");
+
 
     }    // }        
 }
