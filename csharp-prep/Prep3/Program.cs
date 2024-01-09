c82da095cadee56f3ab5f4c029bf02705157fbf0
lauralandonlovell <roryboo@gmail.com>
2024-01-09 10:30:34 +0100

    putting inner loop inside outer loop

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -11,23 +11,23 @@ class Program
         while (response == "yes")
         {
             Console.WriteLine("say hi");
-        }
-        //importing random number
-        Random random = new Random();
-        int newRandom = random.Next(1,101);
-        Console.WriteLine(newRandom);
+        
+            //importing random number
+            Random random = new Random();
+            int newRandom = random.Next(1,101);
+            Console.WriteLine(newRandom);
 
-        int guess = -1;
-        while (guess != newRandom)
-        {
-            Console.Write("Guess a number: ");
-            guess = int.Parse(Console.ReadLine());
-
-            if (newRandom > guess)
+            int guess = -1;
+            while (guess != newRandom)
             {
-                Console.WriteLine("Guess higher next time.");
-            }
-            else if (newRandom < guess)
+                Console.Write("Guess a number: ");
+                guess = int.Parse(Console.ReadLine());
+
+                if (newRandom > guess)
+                {
+                    Console.WriteLine("Guess higher next time.");
+                }
+                else if (newRandom < guess)
             {
                 Console.WriteLine("Guess lower next time.");
             }
@@ -37,7 +37,7 @@ class Program
             }    
         }
         Console.WriteLine("Would you like to play again? yes or no?");
-        response = Console.ReadLine();
+        string response = Console.ReadLine();
         //while (response == "yes")
         // {
         //     Console.WriteLine("say hi");
