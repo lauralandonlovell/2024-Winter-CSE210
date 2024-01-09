c0859583fa6ee0c1d02b3998957982eca121576f
lauralandonlovell <roryboo@gmail.com>
2024-01-10 00:51:00 +0100

    project loads

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,18 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep4 World!");
+    { 
+        int i = 0;
+        while (i < 11)
+        {
+            Console.WriteLine("Enter a number: ");
+            string response = Console.ReadLine();
+            int number = int.Parse(response);
+            Console.WriteLine(number);
+            i++;
+        }
     }
 }
