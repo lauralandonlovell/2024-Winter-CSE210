d3ed1b49ff291b9802e7d4f4facfda4f51c26bea
lauralandonlovell <roryboo@gmail.com>
2024-01-09 17:41:36 +0100

    resolved error CS103.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -8,42 +8,38 @@ class Program
     {
         
         //continuing to play
-        while (response == "yes")
+        // string response = "";
+        // while (response == "yes")
+        //{
+        //importing random number
+        Random random = new Random();
+        int newRandom = random.Next(1,101);
+        Console.WriteLine(newRandom);
+
+        int guess = -1;
+        while (guess != newRandom)
         {
-            Console.WriteLine("say hi");
-        
-            //importing random number
-            Random random = new Random();
-            int newRandom = random.Next(1,101);
-            Console.WriteLine(newRandom);
+            Console.Write("Guess a number: ");
+            guess = int.Parse(Console.ReadLine());
 
-            int guess = -1;
-            while (guess != newRandom)
+            if (newRandom > guess)
             {
-                Console.Write("Guess a number: ");
-                guess = int.Parse(Console.ReadLine());
-
-                if (newRandom > guess)
-                {
-                    Console.WriteLine("Guess higher next time.");
-                }
-                else if (newRandom < guess)
+                Console.WriteLine("Guess higher next time.");
+            }
+            else if (newRandom < guess)
             {
                 Console.WriteLine("Guess lower next time.");
             }
             else
             {
                 Console.WriteLine("Congratulations! You guessed the magic number!"); 
-            }    
+            }
+            Console.WriteLine("Would you like to play again? yes or no?");    
         }
-        Console.WriteLine("Would you like to play again? yes or no?");
-        string response = Console.ReadLine();
-        //while (response == "yes")
-        // {
-        //     Console.WriteLine("say hi");
+            
+            //string response = Console.ReadLine();      //while (response == "yes")
+            // {
+            //     Console.WriteLine("say hi");
 
-        // }
-        
-    }        
-    
+    }    // }        
 }
