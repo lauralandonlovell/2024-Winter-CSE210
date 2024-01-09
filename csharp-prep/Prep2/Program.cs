6b189f32d63b3b5116fd1a2c9d74c9f003c4e15f
lauralandonlovell <roryboo@gmail.com>
2024-01-09 09:39:04 +0100

    loop working. Play stops after num is guessed.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,62 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string percentage = Console.ReadLine();
+        int gradePercentage = int.Parse(percentage);
+        
+        string letter = "";  
+
+        // if (gradePercentage >= 90)
+        // {
+        //     Console.WriteLine("A"); 
+        // }
+        // else if (gradePercentage >= 80)
+        // {
+        //     Console.WriteLine("B");
+        // }
+        // else if (gradePercentage >= 70)
+        // {
+        //     Console.WriteLine("C");
+        // }
+        // else if (gradePercentage >= 60)
+        // {
+        //     Console.WriteLine("D");
+        // }
+        // else 
+        // {
+        //     Console.WriteLine("F");
+        // }
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is {letter}.");
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry you did not pass, please try again next semester.");
+        }   
     }
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Globalization;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+        Random random = new Random();
+        int newRandom = random.Next(1,101);
+        Console.WriteLine(newRandom);
+
+        int guess = -1;
+        while (guess != newRandom)
+        {
+            Console.Write("Guess a number: ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (newRandom > guess)
+            {
+                Console.Write("Guess higher next time.");
+            }
+            else if (newRandom < guess)
+            {
+                Console.Write("Guess lower next time.");
+            }
+            else
+            {
+                Console.WriteLine("Congratulations! You guessed the magic number!"); 
+            }    
+        }
+        Console.Write("Would you like to play again? ");
+        
+    }        
+    
 }
