5cf770b245df3b478aa36121cd83c0a033abe588
lauralandonlovell <roryboo@gmail.com>
2024-01-10 10:33:05 +0100

    creating job class

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,10 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
-    }
+       Console.WriteLine("Hello Learning2 World!");           
+    }    
 }
