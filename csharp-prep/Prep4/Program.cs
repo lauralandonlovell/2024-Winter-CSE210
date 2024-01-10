066b8c81538ac4e117c927af223e973deed95ccf
lauralandonlovell <roryboo@gmail.com>
2024-01-10 01:43:33 +0100

    loop working, it is identifying list literally

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,18 +1,21 @@
 using System;
 using System.Collections.Generic;
 
-class Program
+
+
+//asking user for numbers
+for (int i = 0; i <= 10; i++)
 {
-    static void Main(string[] args)
-    { 
-        int i = 0;
-        while (i < 11)
-        {
-            Console.WriteLine("Enter a number: ");
-            string response = Console.ReadLine();
-            int number = int.Parse(response);
-            //Console.WriteLine(number);
-            i++;
-        }
-    }
+    List<int> numbers = new List<int>();
+
+    //numbers from user
+    Console.WriteLine("Enter a number: ");
+    string number = Console.ReadLine();
+    int numero = int.Parse(number);
+    Console.WriteLine(numero);
+    
+    //filling list
+    numbers.Add(numero);
+    Console.WriteLine(numbers);
 }
+
