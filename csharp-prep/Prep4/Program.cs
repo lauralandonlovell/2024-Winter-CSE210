156ea96b8529465c8dc354b8d19141700bd0a3e1
lauralandonlovell <roryboo@gmail.com>
2024-01-10 02:32:19 +0100

    changed namespace. changed it back.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,23 +1,28 @@
 using System;
 using System.Collections.Generic;
-
-//instantiated list
-List<int>numbers = new List<int>();
-
-
-for (int i = 0; i <= 9; i++)
+class Program
 {
-    //numbers from user
-    Console.WriteLine("Enter a number: ");
-    int numero = int.Parse(Console.ReadLine());
+    static void Main(string[] args)
+    {
+        //instantiated list
+        List<int>numbers = new List<int>();
 
-    //filling list
-    numbers.Add(numero);
+        for (int i = 0; i <= 9; i++)
+        {
+            //numbers from user
+            Console.WriteLine("Enter a number: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            //filling list
+            numbers.Add(numero);
+        }
+
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+    }
 }
 
-foreach (int number in numbers)
-{
-    Console.WriteLine(number);
-}
 
 
