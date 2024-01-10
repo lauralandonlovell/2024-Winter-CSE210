2bdf70b66f13dbf1df586353b246a77c0b1f24c0
lauralandonlovell <roryboo@gmail.com>
2024-01-10 04:32:40 +0100

    completed functions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+    }
+    static void DisplayWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+    static string DisplayName(string userName)
+    {
+        Console.Write("What is your name? ");
+        userName = Console.ReadLine();
+        return (userName);
+    }
+    static int UserNumber(int favoriteNumber)
+    {
+        Console.Write("What is your favorite number? ");
+        favoriteNumber = int.Parse(Console.ReadLine());
+        return (favoriteNumber);
+    }
+    static int SquareNumber(int favoriteNumber)
+    {
+        int squaredNumber = favoriteNumber * favoriteNumber;
+        return squaredNumber;
+    }
+    static void DisplayResult(string userName, int squaredNumber)
+    {
+        Console.WriteLine(userName, squaredNumber);
     }
 }
