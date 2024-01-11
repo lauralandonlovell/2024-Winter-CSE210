eff4c8b7221b20117ff7fa042b0d0d4dcf0754ae
lauralandonlovell <roryboo@gmail.com>
2024-01-11 20:21:21 +0100

    program call to journalDisplayMenu working

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -7,6 +7,18 @@ public class Journal
         Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         Console.WriteLine("Welcome to the Journal Program!");
         Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    }
+    public void DisplayMenu()
+    {
+        List<string> choices = new List<string>();
+        choices.Add("Write");
+        choices.Add("Display");
+        choices.Add("Load");
+        choices.Add("Save");
+        choices.Add("Quit");
+        //below expression inspired by https://stackoverflow.com/questions/62335522/printing-the-index-of-an-item-in-the-item to achieve number with Menu choice.
+        choices.ForEach(item => Console.WriteLine($"{choices.IndexOf(item)}. {item}"));
+
     }   
 }
 
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -9,18 +9,7 @@ class Program
         
         Journal journal1 = new Journal();
         journal1.DisplayMessage();
-
-        List<string> choices = new List<string>();
-        choices.Add("Write");
-        choices.Add("Display");
-        choices.Add("Load");
-        choices.Add("Save");
-        choices.Add("Quit");
+        journal1.DisplayMenu();
         
-        
-        //below expression inspired by https://stackoverflow.com/questions/62335522/printing-the-index-of-an-item-in-the-item to achieve number with Menu choice.
-
-        choices.ForEach(item => Console.WriteLine($"{choices.IndexOf(item)}. {item}"));
-
     }
 }
