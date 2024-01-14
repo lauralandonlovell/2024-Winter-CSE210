64df96a3f62c8eacc9bcc352b69c8e38155b164f
lauralandonlovell <roryboo@gmail.com>
2024-01-14 20:48:48 +0100

    methods are being called but I am not sure the math is working

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -19,8 +19,25 @@ class Program
         fractionoid1.SetBase(5);
         Console.WriteLine(fractionoid1.GetBase());
 
-        fraction1.GetFractionString();
-        fraction1.GetDecimalValue();
+        Fraction f1 = new Fraction();
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction1.GetDecimalValue());
+        
+        Fraction f2 = new Fraction(5);
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+
+        Fraction f3 = new Fraction(3,4);
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+
+        Fraction f4 = new Fraction(1,3);
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
+
+
+        
+
 
 
 
